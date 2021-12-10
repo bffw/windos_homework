dfa0c54ec7922de1bd38aed57cf77eefb0e13b20
bffw <1250458299@qq.com>
2021-12-10 14:57:51 +0800

    B20031727 黄博 静仓库

---
+++ MathLibrary/MathLibrary/MathLibrary.cpp
@@ -0,0 +1,29 @@
+﻿// MathLibrary.cpp : 定义静态库的函数。
+//
+
+#include "framework.h"
+
+#include "MathLibrary.h"
+
+namespace MathLibrary
+{
+    double Arithmetic::Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double Arithmetic::Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double Arithmetic::Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double Arithmetic::Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
